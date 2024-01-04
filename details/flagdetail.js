3cf72c5cb1cf05364186dc2a0bf9b4b2a7b9ed75
Vaniarumuam17 <vani00712@gmail.com>
2024-01-04 10:47:08 +0530

    3rddaytask

--- details/flagdetail.js
+++ details/flagdetail.js
@@ -3,7 +3,7 @@ var request = new XMLHttpRequest();
 //2nd step: API URL
 //.open method
 //2 parameters
-request.open("GET", "https://restcountries.com/v2/all");
+request.open("GET", "https://restcountries.com/v3.1/all");
 //3rd step : establishing the bridge
 request.send();
 //4th step : once the data sucessfully received from server
